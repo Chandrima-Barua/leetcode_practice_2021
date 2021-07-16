48d0e301001ebb9291d7e5c14498a5f100b8534a
Chandrima Barua <chandrimabarua62065@gmail.com>
2021-07-16 12:43:02 +0600

    16 july,2021

--- roman_to_int.js
+++ roman_to_int.js
@@ -1,25 +1,19 @@
 var romanToInt = function(s) {
 
-   var roman = {I:1, V:5, X:10, L: 50, C:100, D:500, M:1000};
-   var integer = 0;
-   var string_to_array = s.split(''); 
-   var second = 0;
-    
+  var roman = {I:1, V:5, X:10, L: 50, C:100, D:500, M:1000};
+  var prev , sum = 0;
+  var string_to_array = s.split('').reverse(); 
    if((string_to_array.length >= 1) && (string_to_array.length <= 15)){
-   for (var i = 0; i< string_to_array.length ; i++){
-       number = string_to_array[i];
-       second = string_to_array[1];
-       console.log(string_to_array[1]);
-       
-       if(parseInt(roman[string_to_array[0]]) < parseInt(roman[string_to_array[1]])){
-       		integer = parseInt(roman[number]) - integer; 
-           
-       }else{
-           
-       		integer = integer + parseInt(roman[number]); 
-       }		 
-      }
-    }
-    
-    return integer
-};
+     for( var i= 0; i < string_to_array.length ; i++){
+         next = (i > 0) ? string_to_array[i - 1] : '';
+         if((sum <= roman[string_to_array[i]]) || (string_to_array[i] == next)){
+         sum = sum + parseInt(roman[string_to_array[i]]);
+         }
+         else{
+             sum = sum - parseInt(roman[string_to_array[i]]);
+         }
+     }
+   }
+     
+     return sum
+ };
